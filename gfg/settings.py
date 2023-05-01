86b4178d7147563378ac85d9626dd11a98fc5ddf
Himanshu Bhardwaj <himanshu77@gmail.com>
2023-05-01 13:56:40 -0400

    added buttons on home page

--- gfg/settings.py
+++ gfg/settings.py
@@ -20,6 +20,9 @@ EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 EMAIL_PORT = EMAIL_PORT
 
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -29,7 +32,7 @@ SECRET_KEY = 'django-insecure-@h)==497rr#a3$-u)9)z_i0#rztspibpi&qz@lzy5=_cp$+9sm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
